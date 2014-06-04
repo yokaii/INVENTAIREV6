9f534681f208d023850ddeb96422bdc30d2f71c7
yokaii <yokaii@hotmail.fr>
2014-06-04 15:37:23 +0200

    Passage en PDO

--- action2.php
+++ action2.php
@@ -3,35 +3,12 @@ session_start();
 $A = $_SESSION["reference"];
 $B = $_SESSION["zone"];
 $C = $_SESSION["table"];
-include ("db2.php");
+include ("db.php");
 
 //Recupration rsultat requte REF
-$query = "SELECT * FROM $C WHERE REF = '$A' "; 
-
-$result = mysql_query($query) or die("<br>** Error database premiere requete : ".mysql_error()."</b><br>$sql");
-$row = mysql_fetch_row($result);
-$REF =  $row[0];
-$CODE =  $row[1]; 
-$FAM =  $row[2]; 
-$DETAIL =  $row[3]; 
-$FIBRE =  $row[4]; 
-$COULEUR =  $row[5]; 
-$BACK =  $row[6]; 
-$GRS =  $row[7]; 
-$LARG =  $row[8]; 
-$LONG =  $row[9]; 
-$HDIAM =  $row[10]; 
-$PNET =  $row[11]; 
-$PBRUT =  $row[12]; 
-$MARQUE =  $row[13]; 
-$REMARQUE =  $row[14]; 
-$DEFAUT =  $row[15]; 
-$ACTION =  $row[16]; 
-$INT_CONDITION =  $row[17]; 
-$MANDRIN =  $row[18]; 
-$NB =  $row[19]; 
-$DP_CODE =  $row[20]; 
-$COM_DE =  $row[21];
+$sql = $db->query("SELECT * FROM allref WHERE REF = '$A' ");
+$fetch = $sql->fetch();
+extract($fetch);
 
 $CODEinv	=	$_POST['CODEMODIF'];
 $DETAILinv	=	$_POST['DETAILMODIF'];
@@ -59,17 +36,12 @@ else{
 	$DECHET		=	 0 ;  //Le checkbox n'a pas été cochée
 }
 	
-$query1 = "INSERT INTO `inventaire`(`REF`, `CODE`, `FAM`, `DETAIL`, `FIBRE`, `COULEUR`, `BACK`, `GRS`, `LARG`, `LONG`, `HDIAM`, `PNET`, `PBRUT`, `MARQUE`, `REMARQUE`, `DEFAUT`, `ACTION`, `INT_CONDITION`, `MANDRIN`, `NB`, `DP_CODE`, `COM_DE`, `CODEinv`, `DETAILinv`, `FIBREinv`, `COULEURinv`, `BACKinv`, `GRSinv`, `LARGinv`, `LONGinv`, `HDIAMinv`, `PNETinv`, `PBRUTinv`, `MARQUEinv`, `REMARQUEinv`, `DEFAUTinv`, `ACTIONinv`, `INT_CONDITIONinv`, `MANDRINinv`, `NBinv`, `DP_CODEinv`, `COM_INV`, `DECHET`)
+$query1 = $db->query ("INSERT INTO `inventaire`(`REF`, `CODE`, `FAM`, `DETAIL`, `FIBRE`, `COULEUR`, `BACK`, `GRS`, `LARG`, `LONG`, `HDIAM`, `PNET`, `PBRUT`, `MARQUE`, `REMARQUE`, `DEFAUT`, `ACTION`, `INT_CONDITION`, `MANDRIN`, `NB`, `DP_CODE`, `COM_DE`, `CODEinv`, `DETAILinv`, `FIBREinv`, `COULEURinv`, `BACKinv`, `GRSinv`, `LARGinv`, `LONGinv`, `HDIAMinv`, `PNETinv`, `PBRUTinv`, `MARQUEinv`, `REMARQUEinv`, `DEFAUTinv`, `ACTIONinv`, `INT_CONDITIONinv`, `MANDRINinv`, `NBinv`, `DP_CODEinv`, `COM_INV`, `DECHET`)
 VALUES ('$REF','$CODE','$FAM','$DETAIL','$FIBRE','$COULEUR','$BACK','$GRS','$LARG','$LONG','$HDIAM','$PNET','$PBRUT','$MARQUE',
 '$REMARQUE','$DEFAUT','$ACTION','$INT_CONDITION','$MANDRIN','$NB','$DP_CODE','$COM_DE','$CODEinv','$DETAILinv','$FIBREinv','$COULEURinv','$BACKinv',
 '$GRSinv','$LARGinv','$LONGinv','$HDIAMinv','$PNETinv','$PBRUTinv','$MARQUEinv','$REMARQUEinv','$DEFAUTinv','$ACTIONinv',
-'$CONDITIONinv','$MANDRINinv','$NBinv','$B','$COMinv','$DECHET')";
+'$CONDITIONinv','$MANDRINinv','$NBinv','$B','$COMinv','$DECHET')");
 
-$req = mysql_query($query1) or die ("<br>** Error in database table <b>".mysql_error()."</b><br>");
-
-
-
-mysql_close();
 
 ?>
 <html>
