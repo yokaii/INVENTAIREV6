54f452aec881dc3cd60c3395c8ecda72c07818d6
mll32 <rakotoarivony.mamilalaina@yahoo.fr>
2014-06-05 14:34:20 +0200

    Update action_produit_suivant.php

--- action_produit_suivant.php
+++ action_produit_suivant.php
@@ -52,7 +52,10 @@ $filename = "img/$A.jpg";
 			if (file_exists($filename)) {    //Vérifie si le fichier existe et renvoie la bonne photo ou la photo de demande.
 				echo "<img src='img/$A.jpg' width='180px' height='200px'><br><table border='3'>";
 			} else {
-				echo "<img src='img/NOPHOTO.jpg' width='470px' height='200px'><br><table border='3'>";
+				echo "
+				<DIV align='left' STYLE='font-family: Arial Black; color: black; font-size: 20pt; margin-top: -20pt'>
+				 <p>Bobine normalement pas au dépot					 
+				</DIV>";
 			}
               
         $row = $sql->fetch();
@@ -62,11 +65,17 @@ $filename = "img/$A.jpg";
 	 $sql2 = $db->query("SELECT * FROM oldref WHERE REF = $A"); 	
 	 if($sql2->rowCount() > 0)
 		{
-			echo "Bobine normalement pas au dépot<br>";
+			echo "
+				<DIV align='left' STYLE='font-family: Arial Black; color: black; font-size: 20pt; margin-top: -20pt'>
+				 <p>Bobine normalement pas au dépot					 
+				</DIV>";
 			if (file_exists($filename)) {    //Vérifie si le fichier existe et renvoie la bonne photo ou la photo de demande.
 				echo "<img src='img/$A.jpg' width='180px' height='200px'><br><table border='3'>";
 			} else {
-				echo "<img src='img/NOPHOTO.jpg' width='470px' height='200px'><br><table border='3'>";
+				echo "
+				<DIV align='left' STYLE='font-family: Arial Black; color: black; font-size: 20pt; margin-top: -20pt'>
+				 <p>Bobine normalement pas au dépot					 
+				</DIV>";
 			}
               
         $row = $sql2->fetch(); 
